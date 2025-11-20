600b466ff41da2fde9bdab580a21153291f02cff
sahilkumardev <sahilkumar710@outlook.com>
2025-11-21 03:29:12 +0530

    feat: update SiteHeader styles for improved aesthetics and performance

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -30,12 +30,15 @@ export default function SiteHeader() {
     <header className="sticky w-full z-50 left-0 right-0 top-5">
       <div
         className={cn(
-          "bg-foreground/3 backdrop-blur-lg z-50 rounded-full border",
-          "transition-all duration-500 ease-in-out",
+          "bg-surface z-50 rounded-full border border-surface-foreground/10",
+          "transition-all duration-700 ease-in-out",
           "flex items-center justify-between px-4 sm:px-8 py-2 md:py-3 mx-4 sm:mx-auto",
+          "relative",
           scrolled ? "max-w-2xl" : "max-w-3xl"
         )}
       >
+        <span className="absolute bottom-0 left-5 h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
+
         <Link href="/" className="hover:cursor-default select-none">
           <Icons.logo className="size-7.5" />
           <span className="sr-only">{siteConfig.name}</span>
